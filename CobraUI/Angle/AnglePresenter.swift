11fb2075356a1452b1cd07aed5fb45a0485dd995
krishpranav <krisna.pranav@gmail.com>
2023-03-09 19:23:19 +0530

    CobraUI: Angle[AngleInteracotr, AnglePresenter]

--- CobraUI/Angle/AnglePresenter.swift
+++ CobraUI/Angle/AnglePresenter.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 final class AnglePresenter: ObservableObject {
-    @Published var animation: Animation
+    @Published var animation: Animation?
     @Published var value: Double = .zero
-    @Published var range: ClosedRange<Double> = .zero
+    @Published var range: ClosedRange<Double> = .zero ... 360
 }
